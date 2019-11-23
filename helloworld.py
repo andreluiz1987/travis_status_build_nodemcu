df484dbefaabdb0b1c6b9d2936ed845a64429793
andreluiz1987 <andre.coelho@hotmart.com>
2019-11-23 12:08:53 -0300

    upload

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import time
+
+def main():
+    print "helloworld"
+    # time.sleep(3)
+
+if __name__ == "__main__":
+    main()
---
+++ semaphore_xecute.py
@@ -0,0 +1,29 @@
+import sys
+import requests
+import threading
+
+success_build_api = "https://api-travis-build.herokuapp.com/api/v1/mcu/actions/success"
+failed_build_api = "https://api-travis-build.herokuapp.com/api/v1/mcu/actions/running"
+running_build_api = "https://api-travis-build.herokuapp.com/api/v1/mcu/actions/failed"
+
+def post_url(url):     
+    r = requests.post(url)   
+    print(r) 
+    if r.status_code != 200:
+        requests.post(url)
+
+
+def main(argv):
+    status = argv   
+    if status == "success":
+        post_url(success_build_api)
+    elif status == "failure":
+        post_url(failed_build_api)
+    elif status == "building":
+        post_url(running_build_api)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: python error [success/failure/building]')
+        exit(1)    
+    main(sys.argv[1])
